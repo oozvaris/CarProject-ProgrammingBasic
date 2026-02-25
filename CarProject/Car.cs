06fc34af3f8a2342a264d18685b48da3e7f163f7
Gelisim University <bim@gelisim.edu.tr>
2026-02-25 11:49:35 +0300

    car update ig :P

--- CarProject/Car.cs
+++ CarProject/Car.cs
@@ -21,7 +21,31 @@ namespace CarProject
             Console.WriteLine("Car created: " + Brand + " in " + Color);
         }
 
+        public void Accelerate(int Amount)
+        {
+            Speed += Amount;
+            Console.WriteLine($"Accelereating... Current speed: {Speed}  km/h");
 
 
+        }
+
+        public void Brake(int Amount)
+
+        {
+            Speed -= Amount;
+            if (Speed < 0)
+                Speed = 0;
+            Console.WriteLine($"Braking... Current speed: {Speed} km/h");
+
+        }
+
+        public void Stop()
+        {
+            Speed = 0;
+            Console.WriteLine("stopped car");
+        }
     }
+
+
+
 }
--- CarProject/Program.cs
+++ CarProject/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Car myCar = new Car("Toyota", "Red");
+            Car myCar = new Car("Toyota", "Blue");
+
+            myCar.Accelerate(50);
+
+            Console.ReadLine();
+
+            myCar.Accelerate(10);
+
+            Console.ReadLine();
+
+            myCar.Brake(10);
+
+            Console.ReadLine();
+
+            myCar.Stop();
 
             Console.ReadLine();
         }
